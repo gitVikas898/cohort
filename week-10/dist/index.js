f5e50f3cb0305c0e663ef2914d07f4fd81ef157b
Vikas <vikas.kumar.singh1198@gmail.com>
2025-03-27 10:11:27 +0530

    Setup typescript project for postgres sql

---
+++ week-10/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+exports.greet = greet;
+console.log((0, exports.greet)("Vikas"));
